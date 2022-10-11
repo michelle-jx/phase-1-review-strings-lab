266947bdd23b427e3e75cbb295fed3cad584de35
Michelle Too <mjxtcodes@gmail.com>
2022-10-11 10:00:26 -0700

    commit tree a811f6c3effc16d88e400fca6601b5ca8ec87507

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = `Grace Hopper`
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser[0]}!`
